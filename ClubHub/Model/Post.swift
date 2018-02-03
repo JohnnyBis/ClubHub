795008db78695ffbc78fd9aa1d83188499220dac
JohnnyBis <gianmariabiselli@gmail.com>
2018-02-03 17:44:52 -0500

    Update

--- ClubHub/Model/Post.swift
+++ ClubHub/Model/Post.swift
@@ -7,8 +7,10 @@
 //
 
 import Foundation
+import FirebaseAuth
+import Firebase
 
-class Post {
+struct Post {
     var imageUrl: String?
     var userName: String?
     var postBody: String?
@@ -24,6 +26,40 @@ class Post {
         self.postTitle = title
         self.likes = likes
         self.documentID = documentID
+
+    }
+    
+    init(url: URL?){
+        self.imageUrl = url?.absoluteString
+    }
+    
+    static func fetchData(_ postList: [Post], completionBlock: @escaping (_ post: Post?, _ error: String?) -> Void){
+        if Auth.auth().currentUser != nil{
+            DataService.ds.REF_POSTS.addSnapshotListener { (querySnapshot, error) in
+                guard let postChanges = querySnapshot else{
+                    print("Error fetching document: \(error!)")
+                    return
+                }
+                postChanges.documentChanges.forEach({ (diff) in
+                    if diff.type == .added || diff.type == .modified || diff.type == .removed{
+                        let documentID = diff.document.documentID
+                        let postBody = diff.document.data()["Body"] as? String
+                        let title = diff.document.data()["Title"] as? String
+                        let likes = diff.document.data()["Likes"] as? Int
+                        let userName = diff.document.data()["User Name"] as? String
+                        let url = diff.document.data()["Url"] as? String
+                        let post = Post(postBody: postBody, title: title, url: url, likes: likes, documentID: documentID, userName: userName)
+                        completionBlock(post, nil)
+    
+                    }
+                })
+                
+            }
+            
+        }else{
+            let errorMessage = "Data fecth error: user could not be authenitcated."
+            completionBlock(nil, errorMessage)
+        }
         
     }
     
